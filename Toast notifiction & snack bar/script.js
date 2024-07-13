541570e53199040a48d86f8f9c8ac9e8d8028323
Deepali Verma <deepaliverma064@gmail.com>
2024-07-13 22:04:08 +0530

    javascript added

--- Toast notifiction & snack bar/script.js
+++ Toast notifiction & snack bar/script.js
@@ -3,13 +3,20 @@ let successMsg = `<i class="fa-solid fa-circle-check"></i> Successfully submitte
 let failedsMsg =  `<i class="fa-solid fa-circle-xmark"></i> Failed! please fix the error.`
 let invalidMsg = `<i class="fa-solid fa-circle-exclamation"></i> Invalid input , check again.`
 
-function showToast(msg){
+function showToast(msg) {
     let toast = document.createElement("div");
-    toast.classList.add("toast")
-    toast.innerHTML= msg;
-    toastBox.appendChild(toast)
+    toast.classList.add("toast");
+    toast.innerHTML = msg;
+    toastBox.appendChild(toast);
 
-    if(msg.inculdes()){
-        
+    if (msg.includes('error')) {
+        toast.classList.add("error");
     }
+    if (msg.includes('Invalid')) {
+        toast.classList.add("invalid");
+    }
+
+    setTimeout(() => {
+        toast.remove();
+    }, 6000);
 }
