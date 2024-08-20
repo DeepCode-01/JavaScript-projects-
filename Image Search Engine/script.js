9e658c1bbe1fbb2d596066f4cd51a9452da679a1
Deepali Verma <deepaliverma064@gmail.com>
2024-08-20 14:56:04 +0530

    javascript aaded

--- Image Search Engine/script.js
+++ Image Search Engine/script.js
@@ -9,16 +9,38 @@ let page = 1;
 
 async function serachImg() {
     keyword = searchBox.value;
-    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}`;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=12`;
 
     const response = await fetch(url);
     const data = await response.json();
 
-    console.log (data);
+    if(page=== 1){
+        searchResult.innerHTML="";
+    }
+
+    const results = data.results;
+    results.map((results)=>{
+        const image = document.createElement('img');
+        image.src= results.urls.small;
+
+        const imageLink = document.createElement("a");
+        imageLink.href = results.links.html;
+        imageLink.target = "_blank"
+
+        imageLink.appendChild(image)
+        searchResult.appendChild(imageLink)
+    })
+
+    showMore.style.display = "block"
 }
 
 searchForm.addEventListener("submit", (e)=>{
     e.preventDefault();
     page =1;
+    serachImg()
+})
+
+showMore.addEventListener("click",()=>{
+    page++;
     serachImg()
 })
