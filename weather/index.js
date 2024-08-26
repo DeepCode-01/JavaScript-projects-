cd6adac4f4cb003696e397e9375d5efb2c802c9e
Deepali Verma <deepaliverma064@gmail.com>
2024-08-26 14:09:39 +0530

    changes

--- weather/index.js
+++ weather/index.js
@@ -1,5 +1,5 @@
 const apiKey = "14278239589d04a0dbdcd5f580d274cd";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=indore";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
@@ -7,7 +7,7 @@ const weatherIcon = document.querySelector(".weather-icon");
 
 async function checkWeather(city) {
   try {
-    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+    const response = await fetch(`${apiUrl}${city}&appid=${apiKey}`);
     
     if (response.status === 404) {
       document.querySelector(".error").style.display = "block";
@@ -15,22 +15,23 @@ async function checkWeather(city) {
     } else {
       const data = await response.json();
 
-      console.log(data)
+      console.log(data);
       
       document.querySelector(".city").innerHTML = data.name;
       document.querySelector(".temp").innerHTML = data.main.temp + "°C";
       document.querySelector(".humidity").innerHTML = Math.round(data.main.humidity) + "%";
       document.querySelector(".wind").innerHTML = data.wind.speed + " km/hr";
       
-      if (data.weather[0].main == "Clouds") {
+ 
+      if (data.weather[0].main === "Clouds") {
         weatherIcon.src = "images/clouds.png";
-      } else if (data.weather[0].main == "Clear") {
+      } else if (data.weather[0].main === "Clear") {
         weatherIcon.src = "images/clear.png";
-      } else if (data.weather[0].main == "Rain") {
+      } else if (data.weather[0].main === "Rain") {
         weatherIcon.src = "images/rain.png";
-      } else if (data.weather[0].main == "Drizzle") {
+      } else if (data.weather[0].main === "Drizzle") {
         weatherIcon.src = "images/drizzle.png";
-      } else if (data.weather[0].main == "Mist") {
+      } else if (data.weather[0].main === "Mist") {
         weatherIcon.src = "images/mist.png";
       }
       
