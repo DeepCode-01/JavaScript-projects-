d358f0c89e070973fded8862ec3c3362acc1b55d
Deepali Verma <deepaliverma064@gmail.com>
2024-08-27 14:01:38 +0530

    Tic Tac Toe Game

--- weather/index.js
+++ weather/index.js
@@ -3,11 +3,11 @@ const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q="
 
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
-const weatherIcon = document.querySelector(".weather-icon");
+const weatherIcon = document.querySelector(".weather-icons");
 
 async function checkWeather(city) {
   try {
-    const response = await fetch(`${apiUrl}${city}&appid=${apiKey}`);
+    const response = await fetch(apiUrl + city +` &appid=${apiKey}`);
     
     if (response.status === 404) {
       document.querySelector(".error").style.display = "block";
@@ -15,23 +15,22 @@ async function checkWeather(city) {
     } else {
       const data = await response.json();
 
-      console.log(data);
+      console.log(data)
       
       document.querySelector(".city").innerHTML = data.name;
       document.querySelector(".temp").innerHTML = data.main.temp + "°C";
       document.querySelector(".humidity").innerHTML = Math.round(data.main.humidity) + "%";
       document.querySelector(".wind").innerHTML = data.wind.speed + " km/hr";
       
- 
-      if (data.weather[0].main === "Clouds") {
-        weatherIcon.src = "images/clouds.png";
-      } else if (data.weather[0].main === "Clear") {
+      if (data.weather[0].main == "Clouds") {
+       weatherIcon.src="images/clouds.png"
+      } else if (data.weather[0].main == "Clear") {
         weatherIcon.src = "images/clear.png";
-      } else if (data.weather[0].main === "Rain") {
+      } else if (data.weather[0].main == "Rain") {
         weatherIcon.src = "images/rain.png";
-      } else if (data.weather[0].main === "Drizzle") {
+      } else if (data.weather[0].main == "Drizzle") {
         weatherIcon.src = "images/drizzle.png";
-      } else if (data.weather[0].main === "Mist") {
+      } else if (data.weather[0].main == "Mist") {
         weatherIcon.src = "images/mist.png";
       }
       
@@ -39,7 +38,7 @@ async function checkWeather(city) {
       document.querySelector(".error").style.display = "none";
     }
   } catch (error) {
-    console.error("Error fetching weather data:", error);
+    // console.error("Error fetching weather data:", error);
     document.querySelector(".error").style.display = "block";
     document.querySelector(".weather").style.display = "none";
   }
@@ -48,7 +47,3 @@ async function checkWeather(city) {
 searchBtn.addEventListener("click", () => {
   checkWeather(searchBox.value);
 });
-
-
-
-
