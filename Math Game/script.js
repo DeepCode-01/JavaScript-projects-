ebef5423b1bc578f7030cda330d685041f7b2cf3
Deepali Verma <deepaliverma064@gmail.com>
2024-09-09 14:10:30 +0530

    javascript adding

---
+++ Math Game/script.js
@@ -0,0 +1,54 @@
+let operators = ["+", "-", "*"];
+const startBtn = document.getElementById("start-btn");
+const Questions = document.getElementById("questions");
+
+const controls = document.querySelector(".control-container");
+const result = document.getElementById("result");
+const submitBtn = document.getElementById("submit-btn");
+const errorMassage = document.getElementById("error-msg");
+
+let answerValue;
+let operatorQuestions;
+
+// random value generator
+
+const randomValue = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+
+const questionsGenerator = ()=>{
+    let [num1, num2]= [randomValue(1,20), randomValue(1,20)];
+   
+    let randomOperator = operators[Math.floor(Math.random() * operators.length)]
+
+    if(randomOperator == "-" && num1 > num2){
+        [num1, num2]=[num2, num1]
+    }
+
+    // solve eqaution
+    let solution = eval(`${num1}${randomOperator} ${num2}`)
+   
+    let randomVar = randomValue(1,5);
+    if(randomVar==1){
+        answerValue = num1
+        Questions.innerHTML= `<input type="number" id="input-val" placeholder="?"/> ${randomOperator} ${num2} = ${solution}`
+    }
+    else if (randomVar ==2){
+            answerValue = num2
+        Questions.innerHTML= `${num1} ${randomOperator} <input type="number" id="input-val" placeholder="?"/> = ${solution}`
+    }
+    else if (randomVar==3){
+               answerValue = randomOperator
+               operatorQuestions = true
+        Questions.innerHTML= `${num1} <input type="number" id="input-val" placeholder="?"/> ${num2} = ${solution}`
+    }
+    else{
+            answerValue =  solution
+        Questions.innerHTML= `${num1} ${randomOperator} ${num2} = <input type="number" id="input-val" placeholder="?"/>`
+    }
+
+}
+
+// start game
+
+startBtn.addEventListener("click", ()=>{
+    questionsGenerator();
+})
