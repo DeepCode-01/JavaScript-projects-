21b151c1d6cc5bee97566c53d9a6175797d29019
Deepali Verma <deepaliverma064@gmail.com>
2024-09-10 14:10:47 +0530

    javascript added

--- Math Game/script.js
+++ Math Game/script.js
@@ -10,21 +10,20 @@ const errorMassage = document.getElementById("error-msg");
 let answerValue;
 let operatorQuestions;
 
-// random value generator
-
+// Random value generator
 const randomValue = (min, max) => Math.floor(Math.random() * (max - min)) + min;
 
 const questionsGenerator = () => {
   let [num1, num2] = [randomValue(1, 20), randomValue(1, 20)];
-
   let randomOperator = operators[Math.floor(Math.random() * operators.length)];
 
-  if (randomOperator == "-" && num1 > num2) {
+ 
+  if (randomOperator == "-" && num1 < num2) {
     [num1, num2] = [num2, num1];
   }
 
-  // solve eqaution
-  let solution = eval(`${num1}${randomOperator} ${num2}`);
+  // Solve the equation
+  let solution = eval(`${num1} ${randomOperator} ${num2}`);
 
   let randomVar = randomValue(1, 5);
   if (randomVar == 1) {
@@ -41,40 +40,40 @@ const questionsGenerator = () => {
     answerValue = solution;
     Questions.innerHTML = `${num1} ${randomOperator} ${num2} = <input type="number" id="input-val" placeholder="?"/>`;
   }
-
-  // input check
-
-  submitBtn.addEventListener("click", () => {
-    errorMassage.classList.add("hide");
-    let userInput = document.getElementById("input-val").value;
-
-    if (userInput) {
-      if (userInput == answerValue) {
-        stopGame(`Yippie!! <span>Correct</span> Answer`);
-      } else if (operatorQuestions && !operators.includes(userInput)) {
-        errorMassage.classList.remove("hide");
-        errorMassage.innerHTML = "Please enter a valid operator";
-      } else {
-        stopGame(`Opps!! <span>Wrong</span> Answer`);
-      }
-    } else {
-      errorMassage.classList.remove("hide");
-      errorMassage.innerHTML = "Input can't be empty";
-    }
-  });
 };
 
-// start game
+// user input
 
+submitBtn.addEventListener("click", () => {
+  errorMassage.classList.add("hide");
+  let userInput = document.getElementById("input-val").value;
+
+  if (userInput) {
+    if (userInput == answerValue) {
+      stopGame(`Yippie!! <span>Correct</span> Answer`);
+    } else if (operatorQuestions && !operators.includes(userInput)) {
+      errorMassage.classList.remove("hide");
+      errorMassage.innerHTML = "Please enter a valid operator";
+    } else {
+      stopGame(`Opps!! <span>Wrong</span> Answer`);
+    }
+  } else {
+    errorMassage.classList.remove("hide");
+    errorMassage.innerHTML = "Input can't be empty";
+  }
+});
+
+// Start game
 startBtn.addEventListener("click", () => {
   operatorQuestions = false;
   answerValue = "";
   errorMassage.innerHTML = "";
-  err.classList.add("hide");
-
+  errorMassage.classList.add("hide");
+ 
   controls.classList.add("hide");
   startBtn.classList.add("hide");
   questionsGenerator();
+
 });
 
 const stopGame = (resultText) => {
