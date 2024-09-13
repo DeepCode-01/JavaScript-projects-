2c0620e4e1be3e0de40cb05bff50e55cf90f830b
Deepali Verma <deepaliverma064@gmail.com>
2024-09-13 15:19:59 +0530

    javascript added

--- Pokemon Card Generator/script.js
+++ Pokemon Card Generator/script.js
@@ -1,4 +1,23 @@
 const url = " https://pokeapi.co/api/v2/pokemon/";
+const typeColors = {
+    bug: "#26de81",
+    dragon: "#ffeaa7",
+    electric:"#fed330",
+    fighting: "#30336b",
+    fairy:"#ff0069",
+    fire: "#f0932b",
+    flying: "#81ecec",
+    grass: "#00b894",
+    ground: "#EFB549",
+    ghost:"#a55eea",
+    ice: "#74b9ff",
+    normal : "#95afc0",
+    poison:"#6c5ce7",
+    psychic: "#a29bfe",
+    rock: "#2d3436",
+    water: "#0190FF"
+
+}
 let card = document.getElementById("card");
 let btn = document.getElementById("btn");
 
@@ -20,10 +39,15 @@ let generateCard = (data) => {
   console.log(data);
   const hp = data.stats[0].base_stat;
   const imgSrc = data.sprites.other.dream_world.front_default;
-  const pokeName = data.name;
-  const statAttack = data.stats[1].base_stats;
-  const statDefence = data.stats[2].base_stats;
-  const statSpeed = data.stats[5].base_stats;
+  const pokeName = data.name[0].toUpperCase() + data.name.slice(1);
+  const statAttack = data.stats[1].base_stat;
+  const statDefence = data.stats[2].base_stat;
+  const statSpeed = data.stats[5].base_stat;
+
+//   set theme color based on pokemon 
+
+const themeColor= typeColors[data.types[0].type.name];
+console.log(themeColor)
 
   card.innerHTML = `  
       <p class="hp">
@@ -33,8 +57,7 @@ let generateCard = (data) => {
             <img src=${imgSrc} alt="">
             <h2 class="poke-name">${pokeName}</h2>
             <div class="types">
-                <span>type1</span>
-                <span>type2</span>
+                
             </div>
             <div class="state">
                 <div>
@@ -55,7 +78,28 @@ let generateCard = (data) => {
                 </div>
             </div>
             `;
+
+            appendTypes(data.types);
+         styleCard(themeColor);
 };
+
+let appendTypes = (types)=>{
+
+    types.forEach(item => {
+       let span = document.createElement("SPAN") 
+       span.textContent = item.type.name;
+       document.querySelector(".types").appendChild(span)
+    });
+}
+
+let styleCard = (color)=>{
+    card.style.background= `radial-gradient(
+        circle at 50% 0% , ${color} 36% , #ffffff 37%)`
+
+    card.querySelectorAll(".types span").forEach(typeColors => {
+        typeColors.style.background = color;
+    }) ;   
+}
 
 btn.addEventListener("click", getPokemonData);
 window.addEventListener("load", getPokemonData);
