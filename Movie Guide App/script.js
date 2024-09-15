149564327a827221574952920c4ee97067ca9df2
Deepali Verma <deepaliverma064@gmail.com>
2024-09-15 15:25:22 +0530

    javascript added

--- Movie Guide App/script.js
+++ Movie Guide App/script.js
@@ -1,6 +1,6 @@
 let key = "391a4af5";
 let movieNameRef = document.getElementById("movie-name");
-let serchBtn = document.getElementById("search-btn");
+let searchBtn = document.getElementById("search-btn"); // Corrected typo here
 let result = document.getElementById("result");
 
 let getMovie = () => {
@@ -8,39 +8,45 @@ let getMovie = () => {
   let url = `http://www.omdbapi.com/?t=${movieName}&apikey=${key}`;
 
   if (movieName.length <= 0) {
-    result.innerHTML = `<h3>Please Enter a Movie Name</h3>`;
-  }
-   else {
+    result.innerHTML = `<h3 class="msg"> Please Enter a Movie Name</h3>`;
+  } else {
     fetch(url)
       .then((resp) => resp.json())
       .then((data) => {
-        result.innerHTML = `
-        <div id="info">
-            <img src=${data.Poster} class="poster"/>
-            <div>
-            <h2>${data.Title}</h2>
-            <div class="rating">
-            <img src="/star-removebg-preview.png"/>
-            <h4>${data.imdbRating}</h4>
+        if (data.Response == "True") {
+          result.innerHTML = `
+            <div id="info">
+                <img src=${data.Poster} class="poster"/>
+                <div>
+                    <h2>${data.Title}</h2>
+                    <div class="rating">
+                        <img src="/star-removebg-preview.png" alt="Star rating"/> <!-- Ensure image path is correct -->
+                        <h4>${data.imdbRating}</h4>
+                    </div>
+                    <div class="details">
+                        <span>${data.Rated}</span>
+                        <span>${data.Year}</span>
+                        <span>${data.Runtime}</span>
+                    </div>
+                    <div class="genre">
+                        <div>${data.Genre.split(",").join("</div><div>")}</div>
+                    </div>
+                </div>
             </div>
-            <div class="details">
-            <span>${data.Rated}</span>
-            <span>${data.Year}</span>
-            <span>${data.Runtime}</span>
-            </div>
-            <div class="genre">
-             <div>${data.Genre.split(",").join("</div><div>")}</div>
-            </div>
-            </div>
-            </div>
-            
             <h3>Plot:</h3>
             <p>${data.Plot}</p>
             <h3>Cast:</h3>
-             <p>${data.Actors}</p>
-            `;
+            <p>${data.Actors}</p>
+          `;
+        } else {
+          result.innerHTML = `<h3 class="msg">${data.Error}</h3>`;
+        }
+      })
+      .catch(() => {
+        result.innerHTML = `<h3 class="msg">Error occurred</h3>`;
       });
   }
 };
-serchBtn.addEventListener("click", getMovie)
+
+searchBtn.addEventListener("click", getMovie); 
 window.addEventListener("load", getMovie);
