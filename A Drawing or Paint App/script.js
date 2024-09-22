36b4f04f3f1eb2c71ecd8067cd0ba529bbf21678
Deepali Verma <deepaliverma064@gmail.com>
2024-09-22 16:05:29 +0530

    adding javascript

--- A Drawing or Paint App/script.js
+++ A Drawing or Paint App/script.js
@@ -2,7 +2,9 @@ const canvas = document.querySelector("canvas");
 toolButton = document.querySelectorAll(".tool");
 
 ctx = canvas.getContext("2d");
-let isDrawing = false;
+
+let prevMouseX, prevMouseY, snapShort;
+isDrawing = false;
 brushWidth = 5;
 selectedTool = "brush";
 
@@ -11,21 +13,36 @@ window.addEventListener("load", () => {
   canvas.height = canvas.offsetHeight;
 });
 
+const drawRectangle = (e) => {
+  ctx.strokeRect(
+    e.offsetX,
+    e.offsetY,
+    prevMouseX - e.offsetX,
+    prevMouseY - e.offsetY
+  );
+};
+
 const drawing = (e) => {
   if (!isDrawing) return;
 
-  if (selectedTool == "brush") {
+  if (selectedTool === "brush") {
     // creating line according to the mouse pointer
     ctx.lineTo(e.offsetX, e.offsetY);
     ctx.stroke();
+  } else if (selectedTool === "rectangle") {
+    drawRectangle(e);
   }
 };
 
-const startDraw = () => {
+const startDraw = (e) => {
   isDrawing = true;
+  ctx.putImageData(snapShort, 0, 0);
+  prevMouseX = e.offsetX;
+  prevMouseY = e.offsetY;
   // creating the new path
   ctx.beginPath();
   ctx.lineWidth = brushWidth;
+  snapShort = ctx.getImageData(0, 0, canvas.width, canvas.height);
 };
 
 toolButton.forEach((btn) => {
