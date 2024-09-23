ce890eab097761b784b6b1f69db473f35475e8d0
Deepali Verma <deepaliverma064@gmail.com>
2024-09-23 13:21:08 +0530

    some changes

--- Country Guide App/script.js
+++ Country Guide App/script.js
@@ -15,28 +15,28 @@ searchBtn.addEventListener("click", () => {
       <h2>${data[0].name.common}</h2>
 
       <div class="wrapper">
-      <div class="data.wrapper">
+      <div class="data-wrapper">
       <h4>Capital:</h4>
       <span>${data[0].capital[0]}</span>
       </div>
       </div>
 
        <div class="wrapper">
-      <div class="data.wrapper">
+      <div class="data-wrapper">
       <h4>Continent:</h4>
       <span>${data[0].continents[0]}</span>
       </div>
       </div>
 
        <div class="wrapper">
-      <div class="data.wrapper">
+      <div class="data-wrapper">
       <h4>Population:</h4>
       <span>${data[0].population}</span>
       </div>
       </div>
 
     <div class="wrapper">
-      <div class="data.wrapper">
+      <div class="data-wrapper">
       <h4>Currency:</h4>
       <span>${data[0].currencies[Object.keys(data[0].currencies)].name} - ${
         Object.keys(data[0].currencies)[0]
@@ -45,7 +45,7 @@ searchBtn.addEventListener("click", () => {
       </div>
 
          <div class="wrapper">
-      <div class="data.wrapper">
+      <div class="data-wrapper">
       <h4>Comman Language:</h4>
       <span>${Object.values(data[0].languages)
         .toString()
