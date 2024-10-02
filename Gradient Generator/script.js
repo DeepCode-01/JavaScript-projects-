6ece7074f321ecd0e18851806796198844595bdc
Deepali Verma <deepaliverma064@gmail.com>
2024-10-02 14:49:08 +0530

    adding javascript

--- Gradient Generator/script.js
+++ Gradient Generator/script.js
@@ -1,15 +1,24 @@
 let coloOne = document.getElementById("color-a");
-let coloTwo= document.getElementById("color-b");
+let coloTwo = document.getElementById("color-b");
 
-let currentDirection = "to bottom"
-let outPut = document.getElementById("code")
+let currentDirection = "to bottom";
+let outPut = document.getElementById("code");
 
-const setDirection = (value,_this)=>{
-  let directions = document.querySelectorAll(".buttons botton")
-  console.log(directions)
-  
-  for(let i of directions){
-    i.classList.remove("active")
+const setDirection = (value, _this) => {
+  let directions = document.querySelectorAll(".buttons button");
+
+  for (let i of directions) {
+    i.classList.remove("active");
   }
-  _this.classList.add("active")
-}
+  _this.classList.add("active");
+  currentDirection = value;
+  console.log(currentDirection);
+};
+
+const generateCode = () => {
+  outPut.value = `background-image: linear-gradient(${currentDirection},${coloOne.value}, ${coloTwo.value})`;
+
+  document.getElementsByTagName(
+    "BODY"
+  )[0].style.backgroundImage = `linear-gradient(${currentDirection},${coloOne.value}, ${coloTwo.value})`;
+};
