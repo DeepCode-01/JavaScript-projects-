55a14d295abea7dff8c4fe6b8086b6f7e29a0dfc
Deepali Verma <deepaliverma064@gmail.com>
2024-10-02 16:00:46 +0530

    adding javascript

---
+++ Marvel API App/apiData.js
@@ -0,0 +1,3 @@
+let ts = "1727864873090"
+let publicKey = ""
+let hashVal = ""
---
+++ Marvel API App/script.js
@@ -0,0 +1,9 @@
+let input = document.getElementById("input-box");
+let submit = document.getElementById("submit-button")
+let showContainer = document.getElementById("show-container")
+let listContainer = document.querySelector(".list")
+
+let date = new Date();
+console.log(date.getTime())
+
+const [timestamp, apiKey , hashValue] = [ts, publicKey, hashVal]
