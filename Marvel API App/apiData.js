428bb3123b37eba46a7391033db84e1825e13255
Deepali Verma <deepaliverma064@gmail.com>
2024-10-03 10:43:28 +0530

    adding js

--- Marvel API App/apiData.js
+++ Marvel API App/apiData.js
@@ -1,3 +1,6 @@
-let ts = "1727864873090"
+let ts = "1727928906511"
 let publicKey = ""
-let hashVal = ""
+let hashVal = "e3a26f637f93c7cc7093d628bc36d5e4"
+
+// 57878gh98
+// 9873505jgh
--- Marvel API App/script.js
+++ Marvel API App/script.js
@@ -1,9 +1,22 @@
 let input = document.getElementById("input-box");
-let submit = document.getElementById("submit-button")
-let showContainer = document.getElementById("show-container")
-let listContainer = document.querySelector(".list")
+let submit = document.getElementById("submit-button");
+let showContainer = document.getElementById("show-container");
+let listContainer = document.querySelector(".list");
 
 let date = new Date();
-console.log(date.getTime())
+console.log(date.getTime());
 
-const [timestamp, apiKey , hashValue] = [ts, publicKey, hashVal]
+const [timestamp, apiKey, hashValue] = [ts, publicKey, hashVal];
+
+submit.addEventListener(
+  "click",
+  (getResult = async () => {
+    if (input.value.trim().length < 1) {
+      alert("Input can't be empty");
+    }
+    showContainer.innerHTML = "";
+    const url = `https://gateway.marvel.com:443/v1/public/characters?ts=${timestamp}apikey=${apiKey}&hash=${hashValue}&name=${input.value}`;
+
+    const response = await fetch(url);
+  })
+);
