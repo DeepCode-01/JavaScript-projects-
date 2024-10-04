681ec128cc4c963cd600095375fe546502882580
Deepali Verma <deepaliverma064@gmail.com>
2024-10-04 15:55:47 +0530

    adding js

---
+++ Responsive Card Slider/script.js
@@ -0,0 +1,21 @@
+const swiper = new Swiper('.slider-wrapper', {
+
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+   breakpoints:{
+    0:{
+        
+    }
+   }
+
+  })
