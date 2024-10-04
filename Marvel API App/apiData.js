acb05f1d9b0433e2bed7d01c8536ff0b23d7265c
Deepali Verma <deepaliverma064@gmail.com>
2024-10-04 14:38:02 +0530

    try to connecting api

--- Marvel API App/apiData.js
+++ Marvel API App/apiData.js
@@ -1,6 +1,6 @@
-let ts = "1727928906511"
+let ts = "1727947016747"
 let publicKey = "1c103bef0ddfd3aafc629f8696f9c201"
-let hashVal = "e3a26f637f93c7cc7093d628bc36d5e4"
+let hashVal = "ddfd62bb3148f2b1edddc392f65966e2"
 
 // 57878gh98
 // 9873505jgh
--- Marvel API App/script.js
+++ Marvel API App/script.js
@@ -4,38 +4,60 @@ let showContainer = document.getElementById("show-container");
 let listContainer = document.querySelector(".list");
 
 let date = new Date();
-console.log(date.getTime());
+const timestamp = date.getTime();
 
-const [timestamp, apiKey, hashValue] = [ts, publicKey, hashVal];
+const [ apiKey, hashValue] = [ publicKey, hashVal];
 
-submit.addEventListener(
-  "click",
-  (getResult = async () => {
-    if (input.value.trim().length < 1) {
-      alert("Input can't be empty");
+submit.addEventListener("click", getResult);
+
+async function getResult() {
+    try {
+        // Input validation
+        if (input.value.trim().length < 1) {
+            alert("Input can't be empty");
+            return;
+        }
+
+        showContainer.innerHTML = ""; 
+        
+        const url = `https://gateway.marvel.com:443/v1/public/characters?ts=${timestamp}&apikey=${apiKey}&hash=${hashValue}&name=${input.value}`;
+
+        const response = await fetch(url);
+        const jsonData = await response.json();
+
+        // Check if the API request was successful
+        if (jsonData.code === 200 && jsonData.data && jsonData.data.results) {
+            if (jsonData.data.results.length === 0) {
+                showContainer.innerHTML = '<div class="error">No character found!</div>';
+                return;
+            }
+
+            jsonData.data.results.forEach((element) => {
+                const imgPath = element.thumbnail.path;
+                const imgExtension = element.thumbnail.extension;
+                
+                showContainer.innerHTML += `
+                    <div class="card-container">
+                        <div class="container-img">
+                            <img src="${imgPath}.${imgExtension}" alt="${element.name}"/>
+                        </div>
+                        <div class="character-name">
+                            ${element.name}
+                        </div>
+                        <div class="character-details">
+                            ${element.description || 'No description available'}
+                        </div>
+                    </div>`;
+            });
+        } else {
+            showContainer.innerHTML = '<div class="error">Error fetching data from Marvel API</div>';
+        }
+    } catch (error) {
+        console.error('Error:', error);
+        showContainer.innerHTML = '<div class="error">Something went wrong! Please try again.</div>';
     }
-    showContainer.innerHTML = "";
-    const url = `https://gateway.marvel.com:443/v1/public/characters?ts=${timestamp}apikey=${apiKey}&hash=${hashValue}&name=${input.value}`;
-
-    const response = await fetch(url);
-    const jsonData = await response.json();
-    jsonData.date["results"].forEach((element) => {
-       showContainer.innerHTML = `
-       <div class="card-container">
-       <div class="container-img">
-       <img src="${element.thumbnail["path"] + "." + element.thumbnail["extention"]}"/>
-       </div>
-       <div class="character-name">
-       ${element.name}
-       </div>
-       <div class="character-details">
-       ${element.description}
-       </div>
-       </div>` 
-    });
-  })
-);
-
-window.onload = ()=>{
- getResult()
 }
+
+window.onload = () => {
+ getResult()
+};
