7bf5ccbabfe130bace30295078dff2e27e781914
Deepali Verma <deepaliverma064@gmail.com>
2024-10-08 10:59:35 +0530

    adding js

---
+++ Roll The Dice/script.js
@@ -0,0 +1,14 @@
+let image = [
+  "dice_1.png",
+  "dice_2.png",
+  "dice-3.png",
+  "dice-4.png",
+  "dice-5.png",
+  "dice-6.png",
+];
+
+let dice = document.querySelectorAll("img");
+
+const rollDice = ()=>{
+    
+}
