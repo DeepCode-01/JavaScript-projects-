7d2c1b7cc8eef9536cb5b7eee0d5008e29301c8e
Deepali Verma <deepaliverma064@gmail.com>
2024-10-09 11:10:05 +0530

    javascript added

---
+++ Number Guessing Game/script.js
@@ -0,0 +1,38 @@
+let msg1 = document.getElementById("message1");
+let msg2 = document.getElementById("message2");
+let msg3 = document.getElementById("message3");
+let guess = document.getElementById("gussedNum");
+
+let answer = Math.floor(Math.random() * 100) + 1;
+let no_of_gueeses = 0;
+let gussed_num = [];
+
+const play = () => {
+  user_gusses = guess.value;
+  if (user_gusses < 1 || user_gusses > 100) {
+
+    alert("Please enter number between 1-100");
+  }
+   else {
+    gussed_num.push(user_gusses);
+    no_of_gueeses += 1;
+
+    if (user_gusses < answer) {
+      msg1.textContent = "Your gussed number is too low.";
+      msg2.textContent = "No. of gusses : " + no_of_gueeses;
+      msg3.textContent = "Gussed numbers are : " + gussed_num;
+    } 
+    
+    else if (user_gusses > answer) {
+
+        msg1.textContent = "Your gussed number is too high.";
+        msg2.textContent = "No. of gusses : " + no_of_gueeses;
+        msg3.textContent = "Gussed numbers are : " + gussed_num;
+    }
+    else if (user_gusses == answer){
+        msg1.textContent = "Yippiee! You win.You gussed it right 🎉"
+        msg2.textContent= "The number was:" + answer   
+        msg3.textContent = "You gussed it in: "+ no_of_gueeses + " gusses" 
+    }
+  }
+};
